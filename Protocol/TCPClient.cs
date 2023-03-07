a968db7e59d1e45e5faa7e19a5353b60ef12e43b
0young1999 <chanqwak@naver.com>
2023-03-07 19:31:51 +0900

    패키징

--- Protocol/TCPClient.cs
+++ Protocol/TCPClient.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
-using System.Windows.Forms;
 
 // https://unininu.tistory.com/475
 
---
+++ TEST/SteamAPITest.cs
@@ -0,0 +1,28 @@
+﻿using SteamQueryNet;
+using SteamQueryNet.Models;
+using System.Collections.Generic;
+
+namespace SpecialCampaignSkillCoolDown
+{
+	internal class SteamAPITest
+	{
+		public ServerQuery _serverQuery;
+
+		public void con()
+		{
+			_serverQuery = new ServerQuery();
+			_serverQuery.Connect("220.72.123.7", 27015);
+		}
+
+		public List<Player> Test()
+		{
+			List<Player> players = _serverQuery.GetPlayers();
+			return players;
+		}
+
+		public int Test1()
+		{
+			return _serverQuery.RenewChallenge();
+		}
+	}
+}
